492380b664bc47a1db5b5dada2a15c65982d3d44
Kai Wen <kaiwen98@gmail.com>
2022-12-02 00:57:35 +0800

    feat: added containerization features

--- backup.sql
+++ backup.sql
@@ -7,12 +7,16 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
+CREATE DATABASE dbs_webapp;
+USE dbs_webapp;
+
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `username` varchar(80) NOT NULL,
   `email` varchar(80) NOT NULL,
-  `password-salt` varchar(80) NOT NULL,
-  `id` int NOT NULL
+  `password_salt` varchar(80) NOT NULL,
+  `password_hash` varchar(80) NOT NULL,
+  `id` varchar(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
