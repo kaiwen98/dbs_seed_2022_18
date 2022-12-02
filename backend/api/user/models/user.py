0680c8fcd6c69d7e3c476d5fc3bb858a075ab0ad
Kai Wen <kaiwen98@gmail.com>
2022-12-02 19:05:41 +0800

    fix: use remote db temporarily

--- backend/api/user/models/user.py
+++ backend/api/user/models/user.py
@@ -4,10 +4,10 @@ from config.db import db
 
 
 class User(db.Model):
-    id = db.Column(db.String, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    password_salt = db.Column(db.String)
-    password_hash = db.Column(db.String)
+    id = db.Column(db.String(80), primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_salt = db.Column(db.String(80))
+    password_hash = db.Column(db.String(80))
     email = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
