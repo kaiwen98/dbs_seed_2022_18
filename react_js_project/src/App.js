8bb72da1b0fa01dba1cef9c225e8fec03635f3c2
vivi0035 <vivi0035@e.ntu.edu.sg>
2022-12-03 15:06:43 +0800

    feat: added user details

--- react_js_project/src/App.js
+++ react_js_project/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
-
+import React from 'react';
+import Account_Details from './Pages/Account_Details';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/home' element={<Homepage></Homepage>}></Route>
+        <Route path='/account_details' element={<Account_Details></Account_Details>}></Route>
       </Routes>
     </>
   );
