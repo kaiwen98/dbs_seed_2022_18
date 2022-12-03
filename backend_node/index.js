5954c0c3b6e95ac5f82ba013e53c13ae5bfc29e4
ivan <wun.iong329@gmail.com>
2022-12-03 15:47:20 +0800

    ready API

--- backend_node/index.js
+++ backend_node/index.js
@@ -1,19 +1,13 @@
 const express =require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const cors = require("cors");
-// const transRouter = require('./routes/sched_trans.js');
 const connection = require('./db/connection');
-// import trans from "../models/sched_trans";
-
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// app.use("/", transRouter);
-
-app.get('/',(req, res) => {
-  // console.log("CONN: ", Connection)
+app.get('/getTrans',(req, res) => {
   connection.query( "SELECT * from Bank.ScheduledTransactions", function(err, data, fields){
       if(err) return next(new AppError(err))
       res.status(200).json({
@@ -26,9 +20,8 @@ app.get('/',(req, res) => {
 
 app.post('/newTrans', (req, res, next) => {
   if (!req.body) return next(new AppError("No form data found", 404));
-  const values = [req.body.TransactionID]
-  connection.query( "INSERT INTO Bank.ScheduledTransactions (TransactionID) VALUE(?)", 
-  [values],
+  connection.query( "INSERT INTO Bank.ScheduledTransactions SET ?", 
+  req.body,
   function(err, data, fields){
       if(err) return next(new AppError(err, 500))
       res.status(201).json({
@@ -52,8 +45,6 @@ app.delete('/deleteTrans/:ID', (req, res, next) => {
   })
 })
 
-// db.sequelize.sync().then(() => {
 app.listen(5000, () => {
 console.log("Server running on port 5000");
 });
-// });
