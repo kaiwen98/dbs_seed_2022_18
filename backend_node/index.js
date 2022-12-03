bcc0b08470c4727dc2c3c89661c56a2e84ff0f31
ivan <wun.iong329@gmail.com>
2022-12-03 12:57:07 +0800

    update DB

--- backend_node/index.js
+++ backend_node/index.js
@@ -1,37 +1,37 @@
 import express from "express";
 import cors from "cors";
-// import db from "./models";
 import transRouter from "./routes/sched_trans.js";
 import mysql from 'mysql';
+
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+// db connections
 const connection = mysql.createConnection({
-  host     : process.env.DB_HOST,
-  user     : process.env.DB_USER,
-  password : process.env.DB_PASSWORD,
-  port     : process.env.PORT
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.log(connection);
-    console.log('db connection failed ' + err.stack);
-    return;
-  }
-  console.log('connected to db');
-});
-
-// connection.end();
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    port     : process.env.PORT
+  });
+  
+  connection.connect(function(err) {
+    if (err) {
+      console.log(connection);
+      console.log('db connection failed ' + err.stack);
+      return;
+    }
+    console.log('connected to db');
+  });
+  
+connection.end();
 
 // Routers
 app.get("/", (req, res) => {app.get("/", (req, res) => {
   res.send("this is the test route to make sure server is working")
-})
-  res.send("this is the test route to make sure server is working")
+  })
 })
 
 app.use("/sched_trans", transRouter);
