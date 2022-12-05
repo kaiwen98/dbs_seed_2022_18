48136d60b8abf4c9dbdbfc41d8ee6437196eeefc
GNivTan <info.gnita88520@gmail.com>
2022-12-05 16:25:29 +0800

    testpush

--- backend_node/index.js
+++ backend_node/index.js
@@ -1,7 +1,7 @@
 const express =require("express");
-// const bodyParser = require("body-parser");
 const cors = require("cors");
 const connection = require('./db/connection');
+// testing adding comments
 
 const app = express();
 app.use(express.json());
